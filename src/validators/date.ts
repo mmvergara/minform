68dc7babd6bb392e4a75a713a521a941495882e8
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-01 12:11:40 +0800

    Add LiteDate validator class for date validation

---
+++ src/validators/date.ts
@@ -0,0 +1,81 @@
+import LiteValidator from "../validator";
+
+type DateValidation =
+  | {
+      check: "min" | "max";
+      val: Date;
+      error?: string;
+    }
+  | {
+      check: "atLeast18";
+      error?: string;
+    };
+
+class LiteDate extends LiteValidator<Date> {
+  private validations: DateValidation[] = [];
+  private static date18YearsAgo = new Date(
+    new Date().setFullYear(new Date().getFullYear() - 18)
+  );
+
+  constructor(typeError: string = "is not a valid date") {
+    super(typeError);
+  }
+
+  min(minDate: Date, error?: string): this {
+    this.validations.push({ check: "min", val: minDate, error });
+    return this;
+  }
+
+  max(maxDate: Date, error?: string): this {
+    this.validations.push({ check: "max", val: maxDate, error });
+    return this;
+  }
+
+  atLeast18(error?: string): this {
+    this.validations.push({ check: "atLeast18", error });
+    return this;
+  }
+
+  validate(propertyName: string, value: Date): string[] {
+    const errors: string[] = [];
+    if (!(value instanceof Date)) {
+      errors.push(`${propertyName} ${this.typeError}`);
+      return errors;
+    }
+
+    this.validations.forEach((validation) => {
+      switch (validation.check) {
+        case "min":
+          if (value < validation.val) {
+            errors.push(
+              validation.error ||
+                `${propertyName} should be atleast ${validation.val}`
+            );
+          }
+          break;
+        case "max":
+          if (value > validation.val) {
+            errors.push(
+              validation.error ||
+                `${propertyName} should be less than ${validation.val}`
+            );
+          }
+          break;
+        case "atLeast18":
+          if (value > LiteDate.date18YearsAgo) {
+            errors.push(
+              validation.error ||
+                `${propertyName} should be atleast 18 years old`
+            );
+          }
+          break;
+      }
+    });
+
+    return errors;
+  }
+}
+
+const liteDate = () => new LiteDate();
+
+export default liteDate;
