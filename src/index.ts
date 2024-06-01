fe49c36c0dc65de8b4dd068e5d69d74f0d36eaa5
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-01 12:02:49 +0800

    Remove unused date validator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import string from "./validators/string";
 import number from "./validators/number";
-import date from "./validators/date";
 import schema from "./schema";
 
-export { schema, string, number, date };
+export { schema, string, number };
--- src/validator.ts
+++ src/validator.ts
@@ -1,34 +1,8 @@
-// LiteValidator.ts
-type ValidationRule = {
-  val: number;
-  error?: string;
-};
-
-interface Validations {
-  min?: ValidationRule;
-  max?: ValidationRule;
-  email?: { error?: string };
-  date?: { error?: string };
-}
-
 abstract class LiteValidator<T> {
-  protected validations: Validations = {};
   protected typeError: string;
-
   constructor(typeError: string) {
     this.typeError = typeError;
   }
-
-  min(val: number, error?: string): this {
-    this.validations.min = { val, error };
-    return this;
-  }
-
-  max(val: number, error?: string): this {
-    this.validations.max = { val, error };
-    return this;
-  }
-
   abstract validate(propertyName: string, value: T): string[];
 }
 
